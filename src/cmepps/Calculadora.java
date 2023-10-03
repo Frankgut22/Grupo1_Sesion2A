36e0121583ca4e3c745e8ad3ca36b4b7b147f77a
usuario <usuario@pc20-002>
2023-10-03 10:20:14 +0200

    Clase con metodos

--- src/cmepps/Calculadora.java
+++ src/cmepps/Calculadora.java
@@ -2,4 +2,19 @@ package cmepps;
 
 public class Calculadora {
 
+	public static int suma (int a, int b) {
+		return a+b;
+	}
+	public static int resta (int a, int b) {
+		return a-b;
+	}
+	public static int divide (int a, int b) {
+		if(b==0)
+			return -1;
+		else
+			return a/b;
+	}
+	public static int multiplicar (int a, int b) {
+		return a*b;
+	}
 }
